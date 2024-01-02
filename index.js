04b77f342b98be0ca478e613986cb7db77a0d111
Chae-Lee <katelee1129@gmail.com>
2024-01-02 21:56:42 +0000

    writeToFile functions and inquirer functions added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const team = require("./src/page-template");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,3 +14,19 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+
+function writeToFile(fileName, data) {
+  fs.writeFile (fileName, data, (err) => {
+    console.log ('testing');
+  });
+}
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    console.log('Generating a HTML file...');
+    writeToFile('team.html', team(answers));
+  });
+}
+
+init();
