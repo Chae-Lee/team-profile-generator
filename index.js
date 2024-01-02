6dd27960f67b197fc1030a5ad30aecabbdf48d7a
Chae-Lee <katelee1129@gmail.com>
2024-01-02 22:16:06 +0000

    if/else statement to capture different selection of employee types

--- index.js
+++ index.js
@@ -33,8 +33,43 @@ const questions = [
     type:'input',
     name:'officeNumber',
     message:'Please input your office number'
+  },
+  {
+    type:'list',
+    name:'employees',
+    message:'Please select the type of employee you would like to create a profile of',
+    choices:['Add an Engineer', 'Add an Intern', 'Finish building the team']
   }
 ];
+
+//refactor this code - it has error
+if (questions[5][0] === 'Add an Engineer'){
+  const engineerQuestions = [
+    {
+      type:'input',
+      name:'engineerName',
+      message:'What is the name of the engineer?'
+    },
+    {
+      type:'input',
+      name:'id',
+      message:'What is the employee ID',
+    },
+    {
+      type:'input',
+      name:'email',
+      message:'What is your email address?'
+    },
+    {
+      type:'input',
+      name:'github',
+      message:'Please input GitHub username'
+    }
+  ];
+};
+
+//when the questions[5][0] is selected another array of questions are asked for Engineers
+//when the questions[5][1] is selected another array of questions are asked for Interns
 
 function writeToFile(fileName, data) {
   fs.writeFile (fileName, data, (err) => {
